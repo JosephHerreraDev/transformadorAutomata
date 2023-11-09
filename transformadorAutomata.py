3b27c410bf8cc6540d30916731033648b75ff808
Joseph Herrera <josephherreraleon@gmail.com>
2023-11-09 09:40:46 -0600

    Agregar los estados a la lista

--- transformadorAutomata.py
+++ transformadorAutomata.py
@@ -1,9 +1,6 @@
 lenguaje = input("Lenguaje del AFN: ")
-
 estados = input("Estados del AFN: ")
-
 estadoInicial = estados[0]
-
 estadosFinal = estados[-1]
 
 class Estado:
@@ -14,24 +11,59 @@ class Estado:
             for simbolo in lenguaje:
                 transicion = input("Transiciones del estado " + estado + " si entra " + simbolo + ": ")
                 self.transiciones[estado, simbolo] = transicion
-        print(self.transiciones)
+
     def regresarTransiciones(self):
         return self.transiciones
 
 estado = Estado(lenguaje, estados)
+tablaTransicionesResumida = []
 tablaTransiciones = []
 for estadoLocal in estados:
     for simbolo in lenguaje:
-        tablaTransiciones.append(estado.regresarTransiciones()[estadoLocal, simbolo])
+        tablaTransiciones.append(estado.regresarTransiciones())
+        tablaTransicionesResumida.append(estado.regresarTransiciones()[estadoLocal, simbolo])
 
-print("Estado\t", end="")
+print("Estado\t", end="|")
 for simbolo in lenguaje:
-    print(simbolo + "\t", end="")
+    print(simbolo + "\t", end="|")
 print()
+x = 0
 for estado in estados:
-    print(estado + "\t", end="")
+    print(estado + "\t", end="|")
     for simbolo in lenguaje:
-        print(tablaTransiciones.pop(0) + "\t", end="")
+        print(tablaTransicionesResumida[x] + "\t", end="|")
+        x = x + 1
     print()
-   
+
+diccionario_destino = {}
+
+tablaTransicionesAFD = []
+tablaEstadosAFD = []
+
+for elemento in tablaTransiciones:
+    diccionario_destino = elemento
+for simbolo in lenguaje:
+    tablaTransicionesAFD.append(diccionario_destino[estadoInicial, simbolo])
+
+tablaEstadosAFD.append(estadoInicial)
+tablaCombinada = []
+for simbolo in lenguaje:
+    for elemento in tablaTransicionesAFD:
+        if elemento not in tablaEstadosAFD:
+            tablaEstadosAFD.append(elemento)
+            for estado in elemento:
+                print(estado + " : " + diccionario_destino[estado, simbolo])
+                tablaCombinada.append(diccionario_destino[estado, simbolo])
+                print(tablaCombinada)
+print(tablaEstadosAFD)
+print(tablaTransicionesAFD)
+
+# * 1. Carga el estado inicial
+# * 2. Leer las transiciones que resultan
+# * 3. Mientras uno de los estados no este en la lista de estados principales, agregarlo
+# 4. Genera las transiciones conbinando las que ya tiene
+# 5. Poner transiciones que ya tiene
+# 6. Regresar al paso 2
+
+
 input("Presione enter para continuar...")
